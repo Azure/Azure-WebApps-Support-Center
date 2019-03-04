f5fbddce79a5e03d802e74bc88fea88460391f25
Puneet Gupta <puneetg@microsoft.com>
2019-03-04 14:05:22 +0530

    highlight interesting messages in logs

--- AngularApp/projects/app-service-diagnostics/src/app/shared/components/daas/cpu-monitoring/cpu-monitoring-activity/cpu-monitoring-activity.component.ts
+++ AngularApp/projects/app-service-diagnostics/src/app/shared/components/daas/cpu-monitoring/cpu-monitoring-activity/cpu-monitoring-activity.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, OnChanges, SimpleChange} from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChange } from '@angular/core';
 import { SiteDaasInfo } from '../../../../models/solution-metadata';
 import { ActiveMonitoringSession } from '../../../../models/daas';
 
@@ -16,9 +16,23 @@ export class CpuMonitoringActivityComponent implements OnInit, OnChanges {
   @Input() siteToBeDiagnosed: SiteDaasInfo;
   @Input() activeMonitoringSession: ActiveMonitoringSession;
 
+  highlightPhrases: string[] = ["threshold", "dump", "killed"];
+
   ngOnInit() {
   }
 
   ngOnChanges() {
+  }
+
+  formatInstanceLogs(logs: string) {
+    let logsArray = logs.split("\r\n");
+
+    for (let index = 0; index < logsArray.length; index++) {
+      if (this.highlightPhrases.some(function(v) { return logsArray[index].indexOf(v) >= 0; })) {
+        logsArray[index] = "<strong>" + logsArray[index] + "</strong>";
+      }
+    }
+    return logsArray.join("\r\n");
+
   }
 }
