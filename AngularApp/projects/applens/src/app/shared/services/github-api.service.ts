18f7932861e50e65de5e2a936364f5ab9da7833a
Yan Xue <xuey@microsoft.com>
2019-03-05 13:37:27 -0800

    Add default value for package.json

--- AngularApp/projects/applens/src/app/shared/services/github-api.service.ts
+++ AngularApp/projects/applens/src/app/shared/services/github-api.service.ts
@@ -26,19 +26,23 @@ export class GithubApiService {
     return this._diagnosticApiService.get<string>(`api/github/package/${detectorId}/statistics/${invokerId}`, true);
   }
 
-  public getSourceReference(id: string, version: string): Observable<string>{
+  public getSourceReference(id: string, version: string): Observable<string> {
     return this.getCommitContent(id, version);
   }
 
-  public getChangelist(id: string): Observable<Commit[]>{
+  public getChangelist(id: string): Observable<Commit[]> {
     return this._diagnosticApiService.getChangelist(id);
   }
 
-  public getCommitContent(id: string, sha: string): Observable<string>{
+  public getCommitContent(id: string, sha: string): Observable<string> {
     return this._diagnosticApiService.getCommitContent(id, sha);
   }
 
-  public  getConfiguration(id: string): Observable<object> {
-    return this._diagnosticApiService.get<string>(`api/github/package/${id}/configuration`, true).pipe(map(conf => JSON.parse(conf)));
+  public getConfiguration(id: string): Observable<object> {
+    return this._diagnosticApiService.get<string>(`api/github/package/${id}/configuration`, true).pipe(
+      map(conf =>{
+        if(conf === "") return {};
+        return JSON.parse(conf);
+      }));
   }
 }
