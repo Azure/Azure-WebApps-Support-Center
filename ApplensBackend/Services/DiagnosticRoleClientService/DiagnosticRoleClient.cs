51b02bb24b03d8dee9b7a249dadfdcc5fa3dfdd6
Hawk Foreste <hawfor@microsoft.com>
2019-08-02 13:02:48 -0700

    Modify diagnostic role pass through condition to get ASC insights

--- ApplensBackend/Services/DiagnosticRoleClientService/DiagnosticRoleClient.cs
+++ ApplensBackend/Services/DiagnosticRoleClientService/DiagnosticRoleClient.cs
@@ -165,7 +165,7 @@ namespace AppLensV3
                 || new Regex("/detectors/[^/]*/statistics").IsMatch(path.ToLower())
                 || path.ToLower().Contains("/diagnostics/publish")
                 || path.ToLower().StartsWith("observer")
-                || path.ToLower().Contains("insights?");
+                || (path.ToLower().Contains("insights?") || path.ToLower().EndsWith("insights"));
         }
 
         private X509Certificate2 GetMyX509Certificate()
