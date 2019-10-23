3da532c06d296d0c8b82605cfbb39cdd8714b518
Ajay Sharma <ajsharm@microsoft.com>
2019-10-23 10:01:36 -0700

    Reuse and refactor

--- AngularApp/projects/app-service-diagnostics/src/app/shared-v2/services/resource.service.ts
+++ AngularApp/projects/app-service-diagnostics/src/app/shared-v2/services/resource.service.ts
@@ -38,11 +38,8 @@ export class ResourceService {
   }
 
   public getPesId(): Observable<string>{
-    if (this._genericArmConfigService){
-      var armResourceConfig = this._genericArmConfigService.getArmResourceConfig(this.resource.id);
-      if (armResourceConfig){
-        return Observable.of(armResourceConfig.pesId);
-      }
+    if (this.armResourceConfig){
+      return Observable.of(this.armResourceConfig.pesId);
     }
     return Observable.of(null);
   }
