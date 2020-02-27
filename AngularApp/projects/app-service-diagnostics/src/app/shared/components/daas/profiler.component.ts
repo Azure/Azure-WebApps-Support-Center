607ea81905936521368a167072aaf506d48f89b5
Puneet Gupta <puneetg@microsoft.com>
2020-02-27 15:05:40 +0530

    Adding completion status in DaaS diagnosers

--- AngularApp/projects/app-service-diagnostics/src/app/shared/components/daas/profiler.component.ts
+++ AngularApp/projects/app-service-diagnostics/src/app/shared/components/daas/profiler.component.ts
@@ -70,25 +70,29 @@ export class ProfilerComponent extends DaasComponent implements OnInit, OnDestro
         this.WizardSteps.push({
             Caption: 'Step 1: Starting Profiler',
             IconType: 'fa-clock-o',
-            AdditionalText: ''
+            AdditionalText: '',
+            CaptionCompleted: 'Step 1: Profiler Started'
         });
 
         this.WizardSteps.push({
             Caption: 'Step 2: Reproduce the issue now',
             IconType: 'fa-user',
-            AdditionalText: 'Profiler trace will stop automatically after 60 seconds unless overriden explicitly'
+            AdditionalText: 'Profiler trace will stop automatically after 60 seconds unless overriden explicitly',
+            CaptionCompleted: 'Step 2: Events captured'
         });
 
         this.WizardSteps.push({
             Caption: 'Step 3: Stopping profiler',
             IconType: 'fa-stop',
-            AdditionalText: ''
+            AdditionalText: '',
+            CaptionCompleted: 'Step 3: Profiler Stopped'
         });
 
         this.WizardSteps.push({
             Caption: 'Step 4: Analyzing profiler trace',
             IconType: 'fa-cog',
-            AdditionalText: ''
+            AdditionalText: '',
+            CaptionCompleted: 'Step 4: Analysis Complete'
         });
 
     }
