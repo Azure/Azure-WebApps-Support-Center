956510b9f39561265244c86f2af9641272953a88
XiaoxuHU <tjhuxiaoxu@gmail.com>
2020-06-30 09:51:01 -0700

    Update API param

--- AngularApp/projects/applens/src/app/modules/dashboard/resource-home/resource-home.component.ts
+++ AngularApp/projects/applens/src/app/modules/dashboard/resource-home/resource-home.component.ts
@@ -76,7 +76,7 @@ export class ResourceHomeComponent implements OnInit {
             return this.detectorsWithSupportTopics;
         }));
 
-        const publicDetectors = this._diagnosticService.getDetectors(null,false);
+        const publicDetectors = this._diagnosticService.getDetectors("",false);
 
         forkJoin(detectorsWithSupportTopics, publicDetectors).subscribe((detectorLists) => {
             detectorLists.forEach((detectorList: DetectorMetaData[]) => {
