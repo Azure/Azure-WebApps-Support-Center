b4bfe64e88d85a88d4709a108d9b391e63ce3d39
XiaoxuHU <tjhuxiaoxu@gmail.com>
2020-07-06 11:43:31 -0700

    Update to remove empty lines

--- AngularApp/projects/diagnostic-data/src/lib/services/parse-resource.service.ts
+++ AngularApp/projects/diagnostic-data/src/lib/services/parse-resource.service.ts
@@ -5,9 +5,6 @@ import { map } from "rxjs/operators";
 import { DiagnosticSiteService } from "./diagnostic-site.service";
 import { ResourceDescriptor } from "../models/resource-descriptor";
 
-
-
-
 @Injectable()
 export class ParseResourceService {
     public supportResources: any[] = [];
@@ -15,25 +12,24 @@ export class ParseResourceService {
     public resourceType: string;
     constructor(private _httpClient: HttpClient, private _diagnosticSiteService: DiagnosticSiteService) { }
 
-    //Only If when parse for main App, then we can differentiate between Web App/Function App/Linux App by DiagnosticSiteService
+    //Only If when parse for Primary Resource, then we can differentiate between Web App/Function App/Linux App by DiagnosticSiteService
     //Todo: add this method into telemetry.service and replace findProductName method  
-    public checkIsResourceSupport(resourceUri: string, isMainApp = true): Observable<string> {
+    public checkIsResourceSupport(resourceUri: string, isPrimaryResource = true): Observable<string> {
         //For cache
         if (this.supportResources.length > 0) {
-            const errorMsg = this.getErrorMsgForSupportType(resourceUri, isMainApp);
+            const errorMsg = this.getErrorMsgForSupportType(resourceUri, isPrimaryResource);
             return of(errorMsg);
         }
 
         return this._httpClient.get<any>('assets/enabledResourceTypes.json').pipe(
             map(response => {
                 this.supportResources = response.enabledResourceTypes;
-                return this.getErrorMsgForSupportType(resourceUri, isMainApp);
-
+                return this.getErrorMsgForSupportType(resourceUri, isPrimaryResource);
             })
         )
     }
 
-    private getErrorMsgForSupportType(resourceUri: string, isMainApp: boolean): string {
+    private getErrorMsgForSupportType(resourceUri: string, isPrimaryResource: boolean): string {
         if (!resourceUri.startsWith('/')) resourceUri = '/' + resourceUri;
         const descriptor = ResourceDescriptor.parseResourceUri(resourceUri);
 
@@ -43,8 +39,6 @@ export class ParseResourceService {
         }
 
         const type = `${descriptor.provider}/${descriptor.types[0]}`;
-
-
         this.resource = this.supportResources.find(resource => type.toLowerCase() === resource.resourceType.toLowerCase());
         this.resourceType = this.resource ? this.resource.searchSuffix : "";
 
@@ -52,14 +46,12 @@ export class ParseResourceService {
             return `Not Support for resource type: ${type}`;
         }
 
-        if (isMainApp) {
+        if (isPrimaryResource) {
             this.checkIsFunctionOrLinux(type);
         }
-
         return "";
     }
-
-
+    
     //Not working for resource integration,since diagnostic service will only fetch for main app,but can be used in telemetry
     private checkIsFunctionOrLinux(type: string): void {
         if (type.toLowerCase() === "microsoft.web/sites") {
@@ -79,10 +71,3 @@ export class ParseResourceService {
         }
     }
 }
-
-
-
-
-
-
-
