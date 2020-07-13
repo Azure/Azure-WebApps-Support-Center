0e65d007ddf40cadeb404b4cd40fa72cc6742434
XiaoxuHU <tjhuxiaoxu@gmail.com>
2020-07-13 11:00:26 -0700

    add detailList,textField into diagnostic-data module

--- AngularApp/projects/diagnostic-data/src/lib/diagnostic-data.module.ts
+++ AngularApp/projects/diagnostic-data/src/lib/diagnostic-data.module.ts
@@ -82,7 +82,7 @@ import { AppDependenciesComponent } from './components/app-dependencies/app-depe
 import { HighchartsChartModule } from 'highcharts-angular';
 import { HighchartsGraphComponent } from './components/highcharts-graph/highcharts-graph.component';
 import { FabNavModule } from './components/fab-nav/fab-nav.module';
-import { FabIconModule, FabChoiceGroupModule, FabSearchBoxModule, FabDropdownModule } from '@angular-react/fabric';
+import { FabIconModule, FabChoiceGroupModule, FabSearchBoxModule, FabDropdownModule, FabDetailsListModule, FabTextFieldModule } from '@angular-react/fabric';
 import { SummaryCardsComponent } from './components/summary-cards/summary-cards.component';
 import { InsightsV4Component } from './components/insights-v4/insights-v4.component';
 import { CardSelectionV4Component } from './components/card-selection-v4/card-selection-v4.component';
@@ -113,7 +113,9 @@ import { MarkdownTextComponent } from './components/markdown-text/markdown-text.
     FabChoiceGroupModule,
     FabSearchBoxModule,
     FabDropdownModule,
-    InViewportModule
+    InViewportModule,
+    FabDetailsListModule,
+    FabTextFieldModule
   ],
   providers: [
     ClipboardService
