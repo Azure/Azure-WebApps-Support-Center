f5663e7935881a325d4110454633ea328f9dc0fc
XiaoxuHU <tjhuxiaoxu@gmail.com>
2020-09-02 15:39:22 -0700

    clean up

--- AngularApp/projects/app-service-diagnostics/src/polyfills.ts
+++ AngularApp/projects/app-service-diagnostics/src/polyfills.ts
@@ -19,24 +19,6 @@
  */
 
 /** IE9, IE10 and IE11 requires all of the following polyfills. **/
-// import 'core-js/es6/symbol';
-// import 'core-js/es6/object';
-// import 'core-js/es6/function';
-// import 'core-js/es6/parse-int';
-// import 'core-js/es6/parse-float';
-// import 'core-js/es6/number';
-// import 'core-js/es6/math';
-// import 'core-js/es6/string';
-// import 'core-js/es6/date';
-// import 'core-js/es6/array';
-// import 'core-js/es6/regexp';
-// import 'core-js/es6/map';
-// import 'core-js/es6/weak-map';
-// import 'core-js/es6/set';
-// import 'core-js/es7/object';
-// import 'core-js/es7/array';
-// import 'core-js/es7/string';
-
 import 'core-js/es/symbol';
 import 'core-js/es/object';
 import 'core-js/es/function';
@@ -51,9 +33,6 @@ import 'core-js/es/regexp';
 import 'core-js/es/map';
 import 'core-js/es/weak-map';
 import 'core-js/es/set';
-// import 'core-js/es7/object';
-// import 'core-js/es7/array';
-// import 'core-js/es7/string';
 import 'core-js/features/url';
 import 'core-js/features/url-search-params';
 
@@ -66,7 +45,7 @@ import 'core-js/es/reflect';
 
 /** Evergreen browsers require these. **/
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
-import 'core-js/es/reflect';
+
 
 
 /**
