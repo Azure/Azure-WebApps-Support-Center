ef7198eecfd65b85b3f9337cb8feea7267ba98a9
XiaoxuHU <tjhuxiaoxu@gmail.com>
2020-09-30 12:16:13 -0700

    Midify regex to ready for both IE and Chrome

--- AngularApp/projects/diagnostic-data/src/lib/utilities/pii-utilities.ts
+++ AngularApp/projects/diagnostic-data/src/lib/utilities/pii-utilities.ts
@@ -16,10 +16,11 @@ export class PIIUtilities {
         return res;
     }
 
+    //Use \\ to replace \,due to js string feature
     public static maskPhone(text: string) {
         let res = text;
         try {
-            const regexStr = "(\+?\d?\d?\d?)([\d\-)\(\s]{10})";
+            let regexStr = "(\\+?\\d?\\d?\\d?)([\\d\\-)\(\\s]{10})";
             const regex = new RegExp(regexStr,"g");
             res = text.replace(regex, " ********** ");
         } catch (e) {
@@ -31,7 +32,7 @@ export class PIIUtilities {
     public static maskEmails(text: string) {
         let res = text;
         try {
-            const regexStr = "(?<=[\w]{1})[\w-\._\+%]*(?=@([\w-_]+)[\.]{0})";
+            const regexStr = "(?<=[\\w]{1})[\\w-\\._\\+%]*(?=@([\\w-_]+)[\\.]{0})";
             const regex = new RegExp(regexStr,"g");
             res = text.replace(regex, s => "*".repeat(s.length));
         } catch (e) {
@@ -43,7 +44,7 @@ export class PIIUtilities {
     public static maskIPV4Address(text: string) {
         let res = text;
         try {
-            const regexStr = "(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])";
+            const regexStr = "(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])";
 
             const regex = new RegExp(regexStr,"g");
             res = text.replace(regex, s => s.split(".")[0] + '.' + s.split(".")[1] + '.' + s.split(".")[2] + '.XXX'
@@ -57,7 +58,7 @@ export class PIIUtilities {
     public static maskPassword(text: string) {
         let res = text;
         try {
-            const regexStr = "(?<=(\bpass\b)|(\bpwd\b)|(\bpassword\b)|(\buserpass\b))[^\w\r\n]+(.+)";
+            const regexStr = "(?<=(\\bpass\\b)|(\\bpwd\\b)|(\\bpassword\\b)|(\\buserpass\\b))[^\\w\\r\\n]+(.+)";
             const regex = new RegExp(regexStr,"gi");
             res = text.replace(regex, ":****");
         } catch (e) {
@@ -69,7 +70,7 @@ export class PIIUtilities {
     public static maskQueryString(text: string) {
         let res = text;
         try {
-            const regexStr = "(?<=https?:\/\/[\w\.-_%]+\?)([\w-\._&%]+=[\w-\._%]+)+";
+            const regexStr = "(?<=https?:\\/\\/[\\w\\.-_%]+\\?)([\\w-\\._&%]+=[\\w-\\._%]+)+";
             const regex = new RegExp(regexStr,"g");
             res = text.replace(regex, "****");
         } catch (e) {
