82d0b7e98e39245695e6a206bf0d0df56c9ead81
XiaoxuHU <tjhuxiaoxu@gmail.com>
2021-01-06 12:50:04 -0800

    block arm call

--- AngularApp/projects/app-service-diagnostics/src/app/home/resolvers/resource.resolver.ts
+++ AngularApp/projects/app-service-diagnostics/src/app/home/resolvers/resource.resolver.ts
@@ -1,13 +1,13 @@
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { ResourceService } from '../../shared-v2/services/resource.service';
 import { ArmResource } from '../../shared-v2/models/arm';
-import { DetectorControlService } from 'diagnostic-data';
+import { DetectorControlService, TelemetryService } from 'diagnostic-data';
 
 @Injectable()
 export class ResourceResolver implements Resolve<Observable<{} | ArmResource>> {
-    constructor(private _resourceService: ResourceService, private _detectorControlService: DetectorControlService) { }
+    constructor(private _resourceService: ResourceService, private _detectorControlService: DetectorControlService,private telemetryService: TelemetryService) { }
     // Live Chat Service is included here so that we ensure an instance is created
 
     resolve(activatedRouteSnapshot: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<{} | ArmResource> {
@@ -16,11 +16,26 @@ export class ResourceResolver implements Resolve<Observable<{} | ArmResource>> {
             this._detectorControlService.setDefault();
         }
 
-        //Try get resourceUri from activatedRoute,if not then get from routerstate
+        //Try get resourceUri from activatedRoute,if not then get from Routerstate
         let resourceUri = activatedRouteSnapshot.parent.url
             .filter(x => x.path !== 'new' && x.path !== 'categories')
             .map(x => x.path)
             .join('/');
+
+        //All dependencies call from below Uri is returning 400, block ARM call
+        const missingApiParamUri = "management.azure.com/?clientOptimizations=undefined&l=en.en-us&trustedAuthority=https:%2F%2Fportal.azure.com&shellVersion=undefined#";
+        if(resourceUri.includes(missingApiParamUri)) {
+            const error = new Error("MissingApiVersionParameter handled at resolver");
+            this.telemetryService.logException(
+                error,
+                "resource.resolver",
+                {
+                    "resourceUri" : resourceUri,  
+                }
+            );
+            return of({});
+        }
+        
         if (!this.checkResourceUri(resourceUri)) {
             const url = state.url;
             const startIndex = url.indexOf("subscriptions/") > -1 ? url.indexOf("subscriptions/") : 0;
@@ -28,10 +43,7 @@ export class ResourceResolver implements Resolve<Observable<{} | ArmResource>> {
 
             resourceUri = url.substring(startIndex, endIndex);
         }
-        if (!this.checkResourceUri(resourceUri)) {
-            const urlFromActivatedRoute = activatedRouteSnapshot.parent.url.join('/');
-            throw new Error(`Empty ResourceUri from resource.resolver;Activated Route: ${urlFromActivatedRoute}; State Url: ${state.url}`);
-        }
+        
 
         return this._resourceService.registerResource(resourceUri);
     }
